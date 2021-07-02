9499fd472bc051a2f8e92c9ba25db037fed825cf
Marcos Trejo <marcostrejo23@gmail.com>
2021-07-01 20:41:02 -0700

    pushing if else

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-var now = moment()
+let now = moment()
 $("#head").text(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+let button = $('.saveBtn');
+
+function currentTime() {
+    let current = moment().hours()
+    let time = $('.time-block');
+
+    time.each(function () {
+      let hour = parseInt($(this).attr('id'))
+      if (hour === current) {
+        $(this).children('.col-sm-10').addClass('present col-sm-10 description')
+      } else if (current > hour) {
+        $(this).children('.col-sm-10').addClass('past col-sm-10 description')
+      } else {
+        $(this).children('.col-sm-10').addClass('future col-sm-10 description')
+      }
+    })
+  }
+  
+  currentTime()
+
+
+  let input=document.getElementsByClassName("savebtn")
