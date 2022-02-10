333ab87d7cba3f8ffa14c63050b11ce837086de2
Marcos Trejo <marcostrejo23@gmail.com>
2022-02-09 19:29:38 -0800

    formatted

--- script.js
+++ script.js
@@ -1,114 +1,128 @@
-let button = $('.saveBtn')
+let button = $(".saveBtn");
 
-
-let now = moment()
+let now = moment();
 $("#head").text(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 function currentTime() {
-    let current = moment().hours()
-    let time = $('.time-block');
+  let current = moment().hours();
+  let time = $(".time-block");
 
-    time.each(function () {
-      let hour = parseInt($(this).attr('id'))
-      if (hour === current) {
-        $(this).children('.col-sm-10').addClass('present col-sm-10 description')
-      } else if (current > hour) {
-        $(this).children('.col-sm-10').addClass('past col-sm-10 description')
-      } else {
-        $(this).children('.col-sm-10').addClass('future col-sm-10 description')
-      }
-    })
-  };
-  currentTime();
+  time.each(function () {
+    let hour = parseInt($(this).attr("id"));
+    if (hour === current) {
+      $(this).children(".col-sm-10").addClass("present col-sm-10 description");
+    } else if (current > hour) {
+      $(this).children(".col-sm-10").addClass("past col-sm-10 description");
+    } else {
+      $(this).children(".col-sm-10").addClass("future col-sm-10 description");
+    }
+  });
+}
+currentTime();
 //to save the information in the text area
-  var container = $(".container")
+var container = $(".container");
 
-container.on("click","#save-btn1", function(event){
+container.on("click", "#save-btn1", function (event) {
   event.preventDefault();
   var info1 = document.getElementById("nine").value;
   console.log(info1);
-  localStorage.setItem("info1",info1)
+  localStorage.setItem("info1", info1);
 });
-container.on("click","#save-btn2", function(event){
+container.on("click", "#save-btn2", function (event) {
   event.preventDefault();
   var info2 = document.getElementById("ten").value;
   console.log(info2);
-  localStorage.setItem("info2",info2)
+  localStorage.setItem("info2", info2);
 });
-container.on("click","#save-btn3", function(event){
+container.on("click", "#save-btn3", function (event) {
   event.preventDefault();
   var info3 = document.getElementById("eleven").value;
   console.log(info3);
-  localStorage.setItem("info3",info3)
+  localStorage.setItem("info3", info3);
 });
-container.on("click","#save-btn4", function(event){
+container.on("click", "#save-btn4", function (event) {
   event.preventDefault();
   var info4 = document.getElementById("twelve").value;
   console.log(info4);
-  localStorage.setItem("info4",info4)
+  localStorage.setItem("info4", info4);
 });
-container.on("click","#save-btn5", function(event){
+container.on("click", "#save-btn5", function (event) {
   event.preventDefault();
   var info5 = document.getElementById("thirteen").value;
   console.log(info5);
-  localStorage.setItem("info5",info5)
+  localStorage.setItem("info5", info5);
 });
-container.on("click","#save-btn6", function(event){
+container.on("click", "#save-btn6", function (event) {
   event.preventDefault();
   var info6 = document.getElementById("fourteen").value;
   console.log(info6);
-  localStorage.setItem("info6",info6)
+  localStorage.setItem("info6", info6);
 });
-container.on("click","#save-btn7", function(event){
+container.on("click", "#save-btn7", function (event) {
   event.preventDefault();
   var info7 = document.getElementById("fifteen").value;
   console.log(info7);
-  localStorage.setItem("info7",info7)
+  localStorage.setItem("info7", info7);
 });
-container.on("click","#save-btn8", function(event){
+container.on("click", "#save-btn8", function (event) {
   event.preventDefault();
   var info8 = document.getElementById("sixteen").value;
   console.log(info8);
-  localStorage.setItem("info8",info8)
+  localStorage.setItem("info8", info8);
 });
-container.on("click","#save-btn9", function(event){
+container.on("click", "#save-btn9", function (event) {
   event.preventDefault();
   var info9 = document.getElementById("seventeen").value;
   console.log(info9);
-  localStorage.setItem("info9",info9)
+  localStorage.setItem("info9", info9);
 });
 
 //to make the saved text appear on refresh.
-function openPage(){
-  if(document.getElementById("nine").value){
-  }else {
-  document.getElementById("nine").value = window.localStorage.getItem('info1');
-  } if (document.getElementById("ten").value){
-  }else {
-  document.getElementById("ten").value = window.localStorage.getItem('info2');
-  } if (document.getElementById("eleven").value){
+function openPage() {
+  if (document.getElementById("nine").value) {
   } else {
-  document.getElementById("eleven").value = window.localStorage.getItem('info3');
-  } if (document.getElementById("twelve").value){
-  } else {
-  document.getElementById("twelve").value = window.localStorage.getItem('info4');
-  } if(document.getElementById("thirteen").value){
-  } else {
-document.getElementById("thirteen").value = window.localStorage.getItem('info5');
-  } if (document.getElementById("fourteen").value){
-  } else {
-document.getElementById("fourteen").value = window.localStorage.getItem('info6');
-  } if (document.getElementById("fifteen").value){
-  } else {
-document.getElementById("fifteen").value = window.localStorage.getItem('info7');
-  } if (document.getElementById("sixteen").value){
-  } else {
-document.getElementById("sixteen").value = window.localStorage.getItem('info8');
-  } if (document.getElementById("seventeen").value){
-  } else {
-document.getElementById("seventeen").value = window.localStorage.getItem('info9');
+    document.getElementById("nine").value =
+      window.localStorage.getItem("info1");
   }
-
-};
+  if (document.getElementById("ten").value) {
+  } else {
+    document.getElementById("ten").value = window.localStorage.getItem("info2");
+  }
+  if (document.getElementById("eleven").value) {
+  } else {
+    document.getElementById("eleven").value =
+      window.localStorage.getItem("info3");
+  }
+  if (document.getElementById("twelve").value) {
+  } else {
+    document.getElementById("twelve").value =
+      window.localStorage.getItem("info4");
+  }
+  if (document.getElementById("thirteen").value) {
+  } else {
+    document.getElementById("thirteen").value =
+      window.localStorage.getItem("info5");
+  }
+  if (document.getElementById("fourteen").value) {
+  } else {
+    document.getElementById("fourteen").value =
+      window.localStorage.getItem("info6");
+  }
+  if (document.getElementById("fifteen").value) {
+  } else {
+    document.getElementById("fifteen").value =
+      window.localStorage.getItem("info7");
+  }
+  if (document.getElementById("sixteen").value) {
+  } else {
+    document.getElementById("sixteen").value =
+      window.localStorage.getItem("info8");
+  }
+  if (document.getElementById("seventeen").value) {
+  } else {
+    document.getElementById("seventeen").value =
+      window.localStorage.getItem("info9");
+  }
+}
 
 openPage();
